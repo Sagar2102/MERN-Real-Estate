8a863144423c498b0d7cfb719bad763e88886c59
Sagar <sagar20032610@gmail.com>
2025-02-11 16:39:37 +0530

    update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -26,8 +26,8 @@ export default function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap ">
-            <span className="text-slate-500">Sagar</span>
-            <span className="text-slate-700">Estate</span>
+            <span className="text-slate-500">Estate</span>
+            <span className="text-slate-700">Hub</span>
           </h1>
         </Link>
         <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
